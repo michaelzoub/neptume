af10d5cebc9a7d7190bc85dc6e6aa58f1d6be0b8
1mints <micacao15@gmail.com>
2025-03-03 22:20:35 -0500

    changed main page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import Combined from './components/mainchat/combined'
-import OpenSign from './components/mainchat/openSign'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import ConnectWalletButton from './components/wallet/connectPrompt'
 import { PrivyProvider } from '@privy-io/react-auth'
-import Modal from './components/modal'
 import { base, polygon, mainnet, optimism, avalanche, worldchain, arbitrum, sepolia, baseSepolia} from 'viem/chains'
 import CheckoutForm from './components/payment/CheckoutForm'
 import CompletePage from './components/payment/CompletePage'
@@ -14,25 +10,6 @@ import { Elements } from "@stripe/react-stripe-js";
 import Home from './components/marketing/combined'
 //
 const stripePromise = loadStripe("pk_test_51Q2bQIGE5A9UAgyNu086KZ7FhBZf5FY14qzqcOOIHpfRGWyv3s42PpQt2vEFxD0npnIdtq1YyRMfT8xCKhcSXoZB00oxrKwmuE");
-
-const mainColor = "#00CC96"
-const secondaryColor = "#00CC96"
-
-const Comb = () => <main className='flex w-full h-screen overflow-hidden justify-center items-center'>
-      <PrivyProvider
-      appId="cm5hc8qro0ec3l1zrv6xwfx99" 
-      config={{
-        defaultChain: base,
-        supportedChains: [base, polygon, mainnet, optimism, avalanche, worldchain, arbitrum, sepolia, baseSepolia] 
-    }}
-    >
-      <Modal color={mainColor} />
-      <ConnectWalletButton color={mainColor} />
-      <OpenSign color={mainColor}  />
-      <Combined color={mainColor} secondary={secondaryColor} />
-    </PrivyProvider>
-</main>
-
 
 const MainPage = () =>  
       <PrivyProvider
