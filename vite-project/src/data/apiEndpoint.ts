e4d1a7df68282b0f04f6f93d36fba478f4578be5
1mints <micacao15@gmail.com>
2025-03-04 20:18:14 -0500

    temp api endpoint change

--- vite-project/src/data/apiEndpoint.ts
+++ vite-project/src/data/apiEndpoint.ts
@@ -1,3 +1,4 @@
 const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT; 
 
-export const apiEndpoint: string = API_ENDPOINT ?? "string"; 
+//export const apiEndpoint: string = API_ENDPOINT ?? "string"; 
+export const apiEndpoint: string = "neptume-production.up.railway.app";
